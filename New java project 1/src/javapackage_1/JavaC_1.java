7c8b4d0afb9be666cb0e4c288ecd90c525d3d1f1
audumbar5555 <112656993+audumbar5555@users.noreply.github.com>
2022-09-02 02:18:50 +0530

    Update JavaC_1.java
    I changed keyword third to fourth

--- New java project 1/src/javapackage_1/JavaC_1.java
+++ New java project 1/src/javapackage_1/JavaC_1.java
@@ -4,7 +4,7 @@ public class JavaC_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is my Third program");
+		System.out.println("This is my fourth program");
 	}
 
 }
