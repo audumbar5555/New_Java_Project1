e122ef2c69abb2314e46aafbd388cfa2f20a967a
audumbar5555 <112656993+audumbar5555@users.noreply.github.com>
2022-09-02 02:01:11 +0530

    Update JavaC_1.java
    also i change "This is my third program"

--- New java project 1/src/javapackage_1/JavaC_1.java
+++ New java project 1/src/javapackage_1/JavaC_1.java
@@ -4,7 +4,7 @@ public class JavaC_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is my second program");
+		System.out.println("This is my Third program");
 	}
 
 }
