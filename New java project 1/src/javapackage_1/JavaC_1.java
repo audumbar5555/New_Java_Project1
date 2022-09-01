27b8c6513cedda2000907e801474471d5189e7dc
Audumbar <Audumbar@LAPTOP-UNTV3BVM>
2022-09-02 01:57:59 +0530

    I have change in printing statement

--- New java project 1/src/javapackage_1/JavaC_1.java
+++ New java project 1/src/javapackage_1/JavaC_1.java
@@ -4,7 +4,7 @@ public class JavaC_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is my first program");
+		System.out.println("This is my second program");
 	}
 
 }
