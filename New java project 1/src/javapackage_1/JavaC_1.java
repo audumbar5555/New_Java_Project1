9d52fd2dd71236e87b28db33c355c2ec4c8fc9fb
Audumbar <Audumbar@LAPTOP-UNTV3BVM>
2022-09-02 02:32:37 +0530

    I have update 10th program

--- New java project 1/src/javapackage_1/JavaC_1.java
+++ New java project 1/src/javapackage_1/JavaC_1.java
@@ -4,7 +4,7 @@ public class JavaC_1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is my third program"); //change program name
+		System.out.println("This is my 10th program"); 
 	}
 
 }
